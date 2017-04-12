210809f91279a0fa4651da57fcb307dc83e70641
Arnulfo Paulino <aguateam@gmail.com>
2017-04-12 06:38:50 +0000

    fixed 502 gateway error

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,7 @@
 from app import app as application
 
+import sys
+sys.path.insert(0,"/app/")
+
 if __name__ == "__main__":
-	application.run(host='0.0.0.0')
+	application.run(host='0.0.0.0', port='5528')
