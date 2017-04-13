093c449754600d7adc821baba5e92346ec7ff542
Arnulfo Paulino <aguateam@gmail.com>
2017-04-13 02:47:48 +0000

    server now answers to /arnulfo

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,12 @@ from flask import Flask
 from app.config import Config
 from app.models import db
 from app.review.controllers import main
+from prefixmid import PrefixMiddleware
 
 app = Flask(__name__)
 
 app.config.from_object(Config)
+app.wsgi_app = PrefixMiddleware(app.wsgi_app, prefix='/arnulfo')
 db.init_app(app)
 db.create_all(app=app)
 
